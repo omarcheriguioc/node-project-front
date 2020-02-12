7a482c0d0336b4aee261a64cbe115c1ff7d0190f
omar cherigui <omar.cherigui@capgemini.com>
2020-02-12 11:57:07 +0100

    design changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Segment, Checkbox, Button } from "semantic-ui-react";
+import { Form, Segment, Checkbox, Button, Card} from "semantic-ui-react";
 
 export default function Login({ login, register }) {
   const [credentials, setCredentials] = useState({
@@ -34,40 +34,23 @@ export default function Login({ login, register }) {
 
   return (
     <>
-      {/* <Segment>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group widths="equal">
-            {!isLogin ? (
-              <Form.Input fluid label="Nom" placeholder="Nom" name="name" value={credentials.name} onChange={handleChange}/>
-            ) : (
-              <h5></h5>
-            )}
-              <Form.Input fluid label="Email" placeholder="Email" name="email" value={credentials.email} onChange={handleChange}/>
-              <Form.Input fluid type="password" label="Mot de passe" placeholder="Mot de passe" name="password" value={credentials.password} onChange={handleChange} />
-            </Form.Group>
-            <Form.Group inline>
-              <Form.Radio label="Login" value="login" checked={isLogin === true} onChange={() => setIsLogin(true)}/>
-              <Form.Radio label="Créer un compte" value="register" checked={isLogin === false} onChange={() => setIsLogin(false)}/>
-            </Form.Group>
-            <Form.Button>
-            {isLogin ? "se connecter" : "créer un compte"}
-            </Form.Button>
+      <Card style={{
+         left: '40%',top: '7%' }}>
+        <Form style={{ margin: '5%'}}>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder='Email' />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder='password' type='password' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='I agree to the Terms and Conditions' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
         </Form>
-      </Segment> */}
-       <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder='First Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder='Last Name' />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
-    </Form.Field>
-    <Button type='submit'>Submit</Button>
-  </Form>
+      </Card>
     </>
   );
 }
